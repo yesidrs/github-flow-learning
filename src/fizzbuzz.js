367148ff3ec688ac04c0b964f78a05559d520b26
yesiddev <yesiddeveloper@gmail.com>
2022-11-02 11:45:19 -0500

    remove comment
    add husky lint

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,11 +1,3 @@
-/*
-write a function that to set a number:
-- if the number is divisible by 3, return "fizz"
-- if the number is divisible by 5, return "buzz"
-- if the number is divisible by 3 and 5, return "fizzbuzz"
-- otherwise, return the number
-*/
-
 export const fizzbuzz = (num) => {
   if (typeof num !== 'number') {
     throw new Error('parameter is not a number')
