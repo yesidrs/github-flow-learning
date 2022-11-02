f6f82d9d1b3ccaf9b8bb5bfd786cfca65d0e8f48
yesiddev <yesiddeveloper@gmail.com>
2022-11-02 09:49:36 -0500

    the real first commit but with husky conf

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -7,17 +7,14 @@ write a function that to set a number:
 */
 
 export const fizzbuzz = (num) => {
-	if (typeof num !== 'number') {
-		throw new Error('parameter is not a number')
-	}
-	if (num % 3 === 0 && num % 5 === 0) {
-		return 'FizzBuzz'
-	}
-	if (num % 3 === 0) {
-		return 'Fizz'
-	}
-	if (num % 5 === 0) {
-		return 'Buzz'
-	}
-	return num
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz'
+  }
+  if (num % 3 === 0) {
+    return 'Fizz'
+  }
+  if (num % 5 === 0) {
+    return 'Buzz'
+  }
+  return num
 }
